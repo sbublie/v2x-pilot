456fb3bd7f0f6a07a4c3207953cd5d4f44eff7ef
Finnchip <sebastian.bublies@daimler.com>
2022-03-17 22:06:41 +0900

    Add signal group handling

--- v2x_pilot/lib/controller.dart
+++ v2x_pilot/lib/controller.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:v2x_pilot/models/lane.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:v2x_pilot/models/signal_group.dart';
 import 'models/lane.dart';
 import 'package:latlong2/latlong.dart' as latlong2;
 
@@ -39,19 +42,65 @@ class BackendController {
           //color: Colors.red,
           width: 5);
 
+      ConnectsWith connectsWith = ConnectsWith(
+          lane?['connects_to']?['lane_id'],
+          lane?['connects_to']?['maneuver_id'],
+          lane?['connects_to']?['signal_group_id']);
+
       Lane newLane = Lane(
           lane?['id'],
           LaneType(lane?['type']),
           nodesLatLong,
-          lane?['signalGroupId'],
-          lane?['ingressApproachId'],
-          lane?['egressApproachId'],
-          lane?['approachType'],
-          lane?['sharedWithId'],
-          lane?['maneuverId'],
+          connectsWith,
+          lane?['ingress_approach_id'],
+          lane?['egress_approach_id'],
+          lane?['approach_type'],
+          lane?['shared_with_id'],
+          lane?['maneuver_id'],
           newPolyline);
       allLanes.add(newLane);
     });
     return LaneCollection(allLanes, refPosition);
+  }
+
+  SignalGroupCollection getSignalGroupCollection(
+      QueryResult result, List<Lane> lanes) {
+    List? signalGroups =
+        result.data?['intersection']?['item']?['signal_groups'];
+    List<SignalGroup> allSignalGroups = [];
+    List<Circle> allCircles = [];
+    signalGroups?.forEach((signalGroup) {
+      lanes.forEach((Lane lane) {
+        if (lane.ingressApproachId != null) {
+          if (lane.connectsWith?.signalGroupId == signalGroup['id']) {
+            LatLng position = lane.nodes.first;
+            SignalGroupState state = SignalGroupState(signalGroup['state']);
+
+            Circle newCircle = Circle(
+                circleId: CircleId(signalGroup['id'].toString()),
+                center: position,
+                fillColor: state.color(),
+                strokeColor: Colors.black,
+                strokeWidth: 2,
+                zIndex: 2,
+                radius: 1);
+
+            SignalGroup newSignalGroup = SignalGroup(
+                signalGroup['id'],
+                state,
+                signalGroup['min_end_time'],
+                signalGroup['max_end_time'],
+                signalGroup['likely_time'],
+                signalGroup['confidence'],
+                position,
+                newCircle);
+            allCircles.add(newCircle);
+            allSignalGroups.add(newSignalGroup);
+          }
+        }
+      });
+    });
+
+    return SignalGroupCollection(allSignalGroups, allCircles);
   }
 }
