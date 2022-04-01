0020fead7268aa11c71a5e96abc8d03af11ac1f5
sbublie <59267927+sbublie@users.noreply.github.com>
2022-04-01 20:09:18 +0200

    Add url dialog

--- v2x_pilot/lib/main.dart
+++ v2x_pilot/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:v2x_pilot/controller.dart';
 import 'package:v2x_pilot/models/signal_group.dart';
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'models/lane.dart';
 import 'controller.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'V2X Pilot',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -35,87 +36,166 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-final httpLink = HttpLink("http://localhost:5000/graphql");
-ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(cache: GraphQLCache(store: HiveStore()), link: httpLink));
-
 class _MyHomePageState extends State<MyHomePage> {
   final Completer<GoogleMapController> _controller = Completer();
+  int _index = 0;
+  String backendURL = "http://127.0.0.1:5000/graphql";
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('V2X-Server URL'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  "Please enter the address under which the V2X-Server is running. Make sure to connect both devices to the same network.",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                    initialValue: backendURL,
+                    onChanged: (input) {
+                      backendURL = input;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'V2X-Server Address',
+                    ))
+              ],
+            ),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 4, 4),
+                child: TextButton(
+                  child: const Text('Save'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    setState(() {
+                      loading = false;
+                    });
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GraphQLProvider(
-        client: client,
-        child: Query(
-            options: QueryOptions(
-              document: gql(readIntersection),
-              variables: {
-                'intersection': 309,
-              },
-              //pollInterval: const Duration(seconds: 10),
-            ),
-            builder: (QueryResult intersectionResult,
-                {VoidCallback? refetch, FetchMore? fetchMore}) {
-              if (intersectionResult.hasException) {
-                return Text(intersectionResult.exception.toString());
-              }
+    HttpLink link = HttpLink(backendURL);
+    ValueNotifier<GraphQLClient> client = ValueNotifier(
+        GraphQLClient(cache: GraphQLCache(store: HiveStore()), link: link));
 
-              if (intersectionResult.isLoading) {
-                return const Text('Loading');
-              }
+    if (loading) {
+      return Container();
+    } else {
+      return GraphQLProvider(
+          client: client,
+          child: Query(
+              options: QueryOptions(
+                document: gql(readIntersection),
+                variables: {
+                  'intersection': 309,
+                },
+                //pollInterval: const Duration(seconds: 10),
+              ),
+              builder: (QueryResult intersectionResult,
+                  {VoidCallback? refetch, FetchMore? fetchMore}) {
+                if (intersectionResult.hasException) {
+                  return Text(intersectionResult.exception.toString());
+                }
 
-              LaneCollection laneCollection =
-                  BackendController().getLaneCollection(intersectionResult);
+                if (intersectionResult.isLoading) {
+                  return const Text('Loading');
+                }
 
-              CameraPosition initialPosition = CameraPosition(
-                target: laneCollection.refPosition,
-                zoom: 20,
-                tilt: 0,
-              );
+                LaneCollection laneCollection =
+                    BackendController().getLaneCollection(intersectionResult);
 
-              Marker refMarker = Marker(
-                  markerId: const MarkerId('refMarker'),
-                  position: laneCollection.refPosition);
-              List<Marker> markers = [refMarker];
-              return GraphQLProvider(
-                  client: client,
-                  child: Query(
-                    options: QueryOptions(
-                      document: gql(readSignalGroups),
-                      variables: {
-                        'intersection': 309,
+                CameraPosition initialPosition = CameraPosition(
+                  target: laneCollection.refPosition,
+                  zoom: 20,
+                  tilt: 0,
+                );
+
+                Marker refMarker = Marker(
+                    markerId: const MarkerId('refMarker'),
+                    position: laneCollection.refPosition);
+                List<Marker> markers = [refMarker];
+                return GraphQLProvider(
+                    client: client,
+                    child: Query(
+                      options: QueryOptions(
+                        document: gql(readSignalGroups),
+                        variables: {
+                          'intersection': 309,
+                        },
+                        pollInterval: const Duration(seconds: 10),
+                      ),
+                      builder: (QueryResult result,
+                          {VoidCallback? refetch, FetchMore? fetchMore}) {
+                        if (result.hasException) {
+                          return Text(result.exception.toString());
+                        }
+
+                        if (result.isLoading) {
+                          return const Text('Loading');
+                        }
+
+                        SignalGroupCollection signalGroupCollection =
+                            BackendController().getSignalGroupCollection(
+                                result, laneCollection.lanes, context);
+
+                        return Scaffold(
+                            extendBody: true,
+                            bottomNavigationBar: Container(
+                              height: 90,
+                              child: FloatingNavbar(
+                                width: 250,
+                                backgroundColor: Colors.white,
+                                selectedItemColor: Colors.blue,
+                                selectedBackgroundColor: Colors.grey[400],
+                                unselectedItemColor: Colors.black,
+                                onTap: (int val) =>
+                                    setState(() => _index = val),
+                                currentIndex: _index,
+                                items: [
+                                  FloatingNavbarItem(
+                                    icon: Icons.map,
+                                    title: 'Map',
+                                  ),
+                                  FloatingNavbarItem(
+                                      icon: Icons.fork_left, title: 'Pilot'),
+                                ],
+                              ),
+                            ),
+                            body: GoogleMap(
+                              markers: markers.toSet(),
+                              polylines: laneCollection.getPolylines().toSet(),
+                              mapType: MapType.satellite,
+                              circles: signalGroupCollection.circles.toSet(),
+                              initialCameraPosition: initialPosition,
+                              onMapCreated: (GoogleMapController controller) {
+                                _controller.complete(controller);
+                              },
+                            ));
                       },
-                      pollInterval: const Duration(seconds: 10),
-                    ),
-                    builder: (QueryResult result,
-                        {VoidCallback? refetch, FetchMore? fetchMore}) {
-                      if (result.hasException) {
-                        return Text(result.exception.toString());
-                      }
-
-                      if (result.isLoading) {
-                        return const Text('Loading');
-                      }
-
-                      SignalGroupCollection signalGroupCollection =
-                          BackendController().getSignalGroupCollection(
-                              result, laneCollection.lanes, context);
-
-                      return Scaffold(
-                        body: GoogleMap(
-                          markers: markers.toSet(),
-                          polylines: laneCollection.getPolylines().toSet(),
-                          mapType: MapType.satellite,
-                          circles: signalGroupCollection.circles.toSet(),
-                          initialCameraPosition: initialPosition,
-                          onMapCreated: (GoogleMapController controller) {
-                            _controller.complete(controller);
-                          },
-                        ),
-                      );
-                    },
-                  ));
-            }));
+                    ));
+              }));
+    }
   }
 }
 
