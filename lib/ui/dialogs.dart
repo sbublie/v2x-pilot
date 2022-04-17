6c0e679de7d62686266f8b5c9a3ede0f0835ec20
Sebastian Bublies <kontakt@s-bublies.de>
2022-04-17 19:36:29 +0200

    Split main file into modules

---
+++ lib/ui/dialogs.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import 'package:provider/provider.dart';
+
+import '/provider/settings.dart';
+
+class ConnectionDialog extends StatefulWidget {
+  final BuildContext context;
+
+  const ConnectionDialog({Key? key, required this.context}) : super(key: key);
+
+  @override
+  State<ConnectionDialog> createState() => _ConnectionDialogState();
+}
+
+class _ConnectionDialogState extends State<ConnectionDialog> {
+  String textValue = "http://127.0.0.1:5000/graphql";
+
+  @override
+  Widget build(BuildContext ctx) {
+    return AlertDialog(
+      title: const Text('No connection'),
+      content: Container(
+        constraints: BoxConstraints(maxWidth: 500),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //Divider(height: 20, color: Colors.grey),
+            const Text(
+              "No conncetion to the V2X-Server! Please enter the address under which the V2X-Server is running. Make sure to connect both devices to the same network.",
+              style: TextStyle(fontSize: 15),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+                initialValue: context.watch<AppSettings>().serverURL,
+                onChanged: (input) {
+                  textValue = input;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'V2X-Server Address',
+                )),
+            Divider(
+              height: 25,
+              color: Colors.grey,
+            ),
+            TextButton(
+              child: const Text('Confirm'),
+              onPressed: () {
+                //setState(() {
+                context.read<AppSettings>().setServerURL(textValue);
+                //Navigator.of(ctx).pop();
+                //});
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsDialog extends StatefulWidget {
+  final BuildContext context;
+
+  const SettingsDialog({Key? key, required this.context}) : super(key: key);
+
+  @override
+  State<SettingsDialog> createState() => _SettingsDialogState();
+}
+
+class _SettingsDialogState extends State<SettingsDialog> {
+  String textValue = "http://127.0.0.1:5000/graphql";
+
+  @override
+  Widget build(BuildContext ctx) {
+    return AlertDialog(
+      title: const Text('Settings'),
+      content: Container(
+        constraints: BoxConstraints(maxWidth: 500),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //Divider(height: 20, color: Colors.grey),
+            const Text(
+              "Enter the address under which the V2X-Server is running. Make sure to connect both devices to the same network.",
+              style: TextStyle(fontSize: 15),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+                initialValue: context.watch<AppSettings>().serverURL,
+                onChanged: (input) {
+                  textValue = input;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'V2X-Server Address',
+                )),
+            Divider(
+              height: 25,
+              color: Colors.grey,
+            ),
+            TextButton(
+              child: const Text('Confirm'),
+              onPressed: () {
+                //setState(() {
+                context.read<AppSettings>().setServerURL(textValue);
+                Navigator.of(ctx).pop();
+                //});
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
