c65fd144af85d282d23184acf3fa924eedff97ba
sbublie <59267927+sbublie@users.noreply.github.com>
2022-04-24 19:55:01 +0200

    Add pilot ui dummy

--- lib/pages/pilot.dart
+++ lib/pages/pilot.dart
@@ -10,9 +10,56 @@ class PilotPage extends StatefulWidget {
 class _PilotPageState extends State<PilotPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Coming soon!',
-      style: TextStyle(fontSize: 20),
-    );
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Approach ID: 12",
+          style: TextStyle(fontSize: 20),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Icon(
+                  Icons.arrow_back_rounded,
+                  size: 150,
+                  color: Colors.red,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.timer_rounded,
+                      size: 50,
+                    ),
+                    Text(
+                      "15s",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.arrow_upward_rounded,
+                    size: 150, color: Colors.green),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.timer_rounded,
+                      size: 50,
+                    ),
+                    Text("10s", style: TextStyle(fontSize: 18))
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
